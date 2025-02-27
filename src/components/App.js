84577ef9093d99e1f09a1e42295155668ed64d98
sushmitha <sushmithathangavel5@gmail.com>
2025-02-27 20:59:16 +0530

    Reorganised the React App

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+//rafce
+
+import React from 'react'
+
+const App = () => {
+  return (
+    //JSX
+    <div>  
+      {/* HOT module replacement --> when you savee file it automaticaly applied in chrome page in react called HMR */}
+      <h1>Hello Everyone</h1>
+    </div>
+  )
+}
+
+export default App
