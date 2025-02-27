c68f115fca40ddbf1412e5de68d67bbe419b0018
sushmitha <sushmithathangavel5@gmail.com>
2025-02-27 21:47:55 +0530

    Installed Node SASS

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    //JSX
-    <div>  
-      {/* HOT module replacement --> when you savee file it automaticaly applied in chrome page in react called HMR */}
-      <h1>Hello Everyone</h1>
-    </div>
-  )
+  {/* HOT module replacement --> when you savee file it automaticaly applied in chrome page in react called HMR */}
+  
+  // Div block is caalled as JSX
+  return <div className='App'>Hello Everyone!</div>
+  
+  //--------------> header>(h1>a{Logo})*nav>ul>li*4>a{Link $}
+
+
 }
 
 export default App
