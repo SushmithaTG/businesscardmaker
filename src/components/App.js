474eb5b5f69d6695143d1e6d898e5b41c06f87fe
sushmitha <sushmithathangavel5@gmail.com>
2025-02-28 15:01:30 +0530

    Added Headers and styles in app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-//rafce
+//rafce  [react arrow function components with export]
 
 import React from 'react'
+import Header from './Header';
 
 const App = () => {
   {/* HOT module replacement --> when you savee file it automaticaly applied in chrome page in react called HMR */}
   
   // Div block is caalled as JSX
-  return <div className='App'>Hello Everyone!</div>
-  
+  return(
+      <div className="App">
+        <Header Dark={true}> Professional Card Maker</Header>
+      </div>
+  );
   //--------------> header>(h1>a{Logo})*nav>ul>li*4>a{Link $}
-
-
 }
 
 export default App;
