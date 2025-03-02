be13ebbcfa41996e3b7e6c2655f211791228a46c
sushmitha <sushmithathangavel5@gmail.com>
2025-03-02 23:54:46 +0530

    Forms Created.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import FormElement from './FormElement';
 
 const Form = () => {
-  return <section className='Form'>Form</section>;
-};
+  return <section className='Form'>
+    <h2>Enter Your Details : </h2>
+    <form>
+        <FormElement ID="Name" Label="Name" />
+        <FormElement ID="Destination" Label="Destination" />
+        <FormElement ID="Phone" Label="Phone" />
+        
+    </form>
+    </section>;
+    
+    };
 
 export default Form
---
+++ src/components/FormElement.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormElement = ({ID,Label,Value}) => {
+  return (
+    <label>
+        <strong>{Label}</strong>
+        <input type='text' name={ID} value={Value}/>
+    </label>
+  )
+}
+
+export default FormElement
