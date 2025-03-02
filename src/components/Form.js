c5c51ed62da6aef2e4cb2fc9926f1babe4af641c
sushmitha <sushmithathangavel5@gmail.com>
2025-03-03 00:03:36 +0530

    Converted repeated FormElement into Array.

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,10 +5,20 @@ const Form = () => {
   return <section className='Form'>
     <h2>Enter Your Details : </h2>
     <form>
-        <FormElement ID="Name" Label="Name" />
+      {/* creating any part js inside html {} */}
+      {["Name","Destination","Phone","Email"].map((field,key)=>(
+        <FormElement ID={field} Label={field} key={key} />
+      ))}
+       
+      {/* INSTEAD OF CREATION THESE 3 LINE I CREATED ABOVE LINE INTO ARRAY TO MAKE IT SIMPLE */}
+
+        {/* <FormElement ID="Name" Label="Name" />
         <FormElement ID="Destination" Label="Destination" />
         <FormElement ID="Phone" Label="Phone" />
-        
+         */}
+
+
+
     </form>
     </section>;
     
