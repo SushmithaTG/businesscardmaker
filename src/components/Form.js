fa5367200c342b8ec4af85af002fcea089aecc1b
sushmitha <sushmithathangavel5@gmail.com>
2025-03-03 22:48:38 +0530

    Implemented the values and fields from Parent values.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import FormElement from './FormElement';
 
-const Form = () => {
+const Form = ({onChange,Values}) => {
   return <section className='Form'>
     <h2>Enter Your Details : </h2>
-    <form>
+    <form onChange={onChange}>
       {/* creating any part js inside html {} */}
-      {["Name","Destination","Phone","Email"].map((field,key)=>(
-        <FormElement ID={field} Label={field} key={key} />
+      {/* ["Name","Destination","Phone","Email"]  Instead of this i created Object.keys(Values) as follows that performs as same As one point of place where all things are
+      managed by App.js  */}
+      {Object.keys(Values).map((field,key)=>(
+        <FormElement 
+        ID={field} 
+        Label={field} 
+        Values={Values[field]} 
+        key={key} 
+        />
       ))}
        
       {/* INSTEAD OF CREATION THESE 3 LINE I CREATED ABOVE LINE INTO ARRAY TO MAKE IT SIMPLE */}
